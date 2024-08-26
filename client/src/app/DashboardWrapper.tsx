13832de84b1f39e28718f17d14354f9ca96eaf20
miha11-hash <d_mihaela@msn.com>
2024-08-26 09:55:16 +0300

    change classes with redux - DashboardWrapper

--- client/src/app/DashboardWrapper.tsx
+++ client/src/app/DashboardWrapper.tsx
@@ -6,10 +6,14 @@ import Sidebar from "@/app/(components)/sidebar/Sidebar";
 import StoreProvider, { useAppSelector } from "./redux";
 
 export function DashboardLayout({children} : {children: React.ReactNode}) {
+  const isSidebarCollapsed = useAppSelector((state)=> state.global.isSidebarCollapsed);
+  const isDarkMode = useAppSelector((state)=> state.global.isDarkMode);
+
   return (
-    <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+    <div className={`${isDarkMode ? "dark" : "light"} flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
         <Sidebar></Sidebar>
-        <main className="flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-72">
+        <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 
+        ${isSidebarCollapsed ? "md:pl-24" : "md:pl-72"}`}>
         <Navbar></Navbar>
         {children}
         </main>
