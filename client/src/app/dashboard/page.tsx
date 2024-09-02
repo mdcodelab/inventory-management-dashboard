33a869dd706ed38bed942fc1c15386c99b6c2c67
miha11-hash <d_mihaela@msn.com>
2024-09-02 19:46:12 +0300

    PurchaseSummary

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,13 +1,15 @@
 import { CheckCircle, Package, Tag, TrendingDown, TrendingUp } from "lucide-react";
 import PopularProducts from "./PopularProducts";
+import PurchaseSummary from "./PurchaseSummary";
+import SalesSummary from "./SalesSummary";
 
 const Dashboard = () => {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid">
       <PopularProducts></PopularProducts>
-      <div className="row-span-3 xl:row-span-6 bg-gray-500"></div>
-      <div className="row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500"></div>
+      <SalesSummary></SalesSummary>
+      <PurchaseSummary></PurchaseSummary>
       <div className="row-span-3 bg-gray-500"></div>
       <div className="row-span-3 xl:row-span-2 bg-gray-500"></div>
       <div className="row-span-3 xl:row-span-2 bg-gray-500"></div>
