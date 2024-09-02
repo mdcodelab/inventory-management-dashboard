538941ab44e3658ffae5b9feaad7a9afee027b2a
miha11-hash <d_mihaela@msn.com>
2024-09-02 15:11:47 +0300

    Rating component

--- client/src/app/(components)/rating/Rating.tsx
+++ client/src/app/(components)/rating/Rating.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { FaStar } from "react-icons/fa";
+import { CiStar } from "react-icons/ci";
 
-function Rating() {
+type RatingProps = {
+  rating: number;
+};
+
+const Rating = ({ rating }: RatingProps) => {
   return (
-    <div>
-      Rating
-    </div>
-  )
-}
+    <>
+      {[1, 2, 3, 4, 5].map((index) => (
+        index <= rating ? <FaStar key={index} style={{ color: "gold", width: "1rem", height: "1rem" }} /> : <CiStar key={index} style={{ color: "lightgray", width: "1rem", height: "1rem" }} />
+      ))}
+    </>
+  );
+};
 
-export default Rating
+export default Rating;
